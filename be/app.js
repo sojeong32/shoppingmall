1bc7086e823263d1da8aee156315755f99a07215
박소정 <thwjd0235@naver.com>
2024-10-25 00:20:54 +0900

    feat: 회원가입 구현

--- be/app.js
+++ be/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const indexRouter = require("./routes/index");
 const app = express();
 
 require("dotenv").config();
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); // req.body가 객체로 인식됨
 
+app.use("/api", indexRouter);
 const mongoURI = process.env.Local_DB_ADDRESS;
 mongoose
   .connect(mongoURI)
